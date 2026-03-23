b94b353641cc56f0131c51967e34f43a368f2bab
Ved Sharma <vedsharmabgt@gmail.com>
2026-03-23 23:30:21 +0530

    Initial commit:data generation setup

---
+++ data_generator.py
@@ -0,0 +1,53 @@
+import random
+import csv
+data=[]
+x=1000
+def generate_human_sample():
+    session_duration=random.randint(20, 60)
+    actions = random.randint(50, 300)
+
+    actions_per_second = actions / session_duration
+    return {
+  "avg_mouse_speed": random.uniform(1,3),
+  "mouse_speed_variance": random.uniform(0.5,2),
+  "click_interval_avg": random.uniform(0.3,1),
+  "click_interval_variance": random.uniform(0.2,0.9),
+  "typing_avg_delay": random.uniform(0.1,0.7),
+  "typing_variance": random.uniform(0.1,0.7),
+  "backspace_count": random.randint(1,10),
+  "scroll_speed": random.uniform(0.5,2),
+  "hesitation_time": random.uniform(0.3,3),
+  "session_duration":session_duration,
+  "actions_per_second":actions_per_second,
+  "label": 1
+}
+for _ in range(1000):
+    data.append(generate_human_sample())
+
+def generate_bot_sample():
+    session_duration = random.randint(5, 20)  # bots act fast
+    actions = random.randint(200, 500)  # high activity
+
+    return {
+        "avg_mouse_speed": random.uniform(2.5, 4),  # faster than humans
+        "mouse_speed_variance": random.uniform(0.01, 0.1),  # VERY low variation
+        "click_interval_avg": random.uniform(0.05, 0.2),  # rapid clicks
+        "click_interval_variance": random.uniform(0.001, 0.05),  # almost constant
+        "typing_avg_delay": random.uniform(0.01, 0.05),  # super fast typing
+        "typing_variance": random.uniform(0.001, 0.02),  # almost no variation
+        "backspace_count": 0,  # bots don’t make mistakes
+        "scroll_speed": random.uniform(2, 5),  # fast scrolling
+        "hesitation_time": random.uniform(0.0, 0.1),  # no hesitation
+        "session_duration": session_duration,
+        "actions_per_second": actions / session_duration,
+        "label": 0
+    }
+for _ in range(1000):
+    data.append(generate_bot_sample())
+
+keys = data[0].keys()
+
+with open("dataset.csv", "w", newline="") as file:
+    writer = csv.DictWriter(file, fieldnames=keys)
+    writer.writeheader()
+    writer.writerows(data)
---
+++ readme.md
@@ -0,0 +1,27 @@
+# Passive Human Verification System
+
+An ML-based system to detect human vs bot behavior without using CAPTCHA.
+
+## Features
+
+* Behavioral analysis (mouse, typing, session)
+* ML-based classification
+* Passive verification (no user interruption)
+
+## Tech Stack
+
+* Python (FastAPI - upcoming)
+* Machine Learning (Scikit-learn)
+* MySQL (planned)
+
+## Current Progress
+
+* [x] Feature design
+* [x] Data generation
+* [ ] Model training
+* [ ] API development
+* [ ] Frontend SDK
+
+## Goal
+
+Replace CAPTCHA with seamless, intelligent verification.
